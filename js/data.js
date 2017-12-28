860779cf869d8aacef4c7086063c8de1f6165227
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2017-12-28 11:18:47 -0500

    Añadiendo data

---
+++ js/data.js
@@ -0,0 +1,78 @@
+var dataFood = {
+  'Bao?':{
+    'Logo':'https://www.facebook.com/baoacomer/photos/a.878359075618840.1073741825.868178746636873/987367018051378/?type=1&theater',
+    'Tipo de comida': 'Comida rápida, Asiática, Apto para vegetarianos, Opciones veganas',
+    'Características':'Comida para llevar, Asientos al aire libre, Asiento, Acceso para silla de ruedas, Sirve alcohol',
+    'Dónde': {
+      'Miraflores':'Calle José domingo Choquehuanca 411'
+    },
+    'Horario de Atención':'L-J de 12:00p.m. a 4:00p.m. y 7:00p.m. a 11:00p.m., y V-D de 12:00p.m. a 11:00p.m.',
+    'Teléfono':'(01) 4834180',
+    'Facebook':'https://www.facebook.com/baoacomer/',
+    'Platos principales':['Shitake S/23.00','Pulled Pork S/23.00','Monbao S/25.00','Crunchy S/11.00','Shikake S/9.00']
+  }
+  'Veggie Pizza':{
+    'Logo':'https://www.facebook.com/VeggiePizzaPE/photos/a.320129321357475.67075.320128634690877/1229926433711088/?type=1&theater',
+    'Tipo de comida': 'Bar de vinos, Pizza, Apto para vegetarianos, Opciones veganas, Opciones sin gluten',
+    'Características':'Comida para llevar, Asiento, Meseros, Estacionamiento privado gratis, Sillitas altas disponibles, Acceso para silla de ruedas, Sirve alcohol, Bar completo, Vino y cerveza, Acepta American Express, Acepta Mastercard, Acepta Visa, Entrega a domicilio, Reservaciones, Wi-Fi gratis, Acepta tarjetas de crédito',
+    'Dónde': {
+      'Miraflores':'Calle Enrique Palacio 350',
+      'Miraflores':'Calle Luis Aria Schereiber 319',
+      'Barranco':'Jirón Colina 112'
+    },
+    'Horario de Atención':'L-D de 12:00p.m. a 0:00a.m.',
+    'Teléfono':'(01) 2475963 / (01) 383-7066 / (01) 4698709',
+    'Facebook':'https://www.facebook.com/VeggiePizzaPE/',
+    'Platos principales':['Veggie Burger S/22.00','Bruschetta S/30.00-S/60.00','Sopa de espárragos S/18.00','Sorbet de verano S/14.00']
+  }
+  'Café A Market':{
+    'Logo':'https://www.facebook.com/272674056509217/photos/a.313629199080369.1073741828.272674056509217/313629202413702/?type=3&theater',
+    'Tipo de comida': 'Café, postres, sandwhich',
+    'Características':'Cafetería',
+    'Dónde': {
+      'Magdalena del Mar':'Avenida Del Ejército 575',
+    },
+    'Horario de Atención':'L-S de 8:00a.m.. a 8:00p.m. D de 8:00a.m. a 5:00p.m.',
+    'Teléfono':'(01) 2786290',
+    'Facebook':'https://www.facebook.com/Caf%C3%A9-A-Market-272674056509217/',
+    'Platos principales':['Parker house rolls S/24.00', 'Pecan pie']
+  },
+  'Beso Francés':{
+    'Logo':'https://www.facebook.com/BesoFrancesCreperia/photos/a.164803686879806.43814.164803416879833/1812777828749042/?type=1&theater',
+    'Tipo de comida': 'Francesa, Café, Comida rápida, Apto para vegetarianos',
+    'Características':'Estacionamiento disponible, Sillitas altas disponibles, Acceso para silla de ruedas, Comida para llevar, Asientos al aire libre, Asiento, Meseros, Estacionamiento en la calle, Acepta Mastercard, Acepta Visa, Wi-Fi gratis, Acepta tarjetas de crédito',
+    'Dónde': {
+      'Miraflores':'Malecón de la Reserva - Parque Intihuatana',
+      'Surco':'Jockey Plaza - Paseo Happyland',
+      'Surquillo':'Open Plaza Angamos',
+    },
+    'Horario de Atención':'L-S de 7:00a.m.. a 11:00p.m.',
+    'Teléfono':'(01) 7178949',
+    'Facebook':'https://www.facebook.com/BesoFrancesCreperia/',
+    'Platos principales':['Crepes salados S/14.00 a S/18.00', 'Crepes dulces S/10.00 a S/17.00','Sandwiches S/13.00 a S/18.00']
+  },
+  'La Bodega Verde':{
+    'Logo':'https://www.facebook.com/labodegaverde/photos/a.146535138733040.33207.146535032066384/846388678747679/?type=1&theater',
+    'Tipo de comida': 'Café, Delicatessen, Apto para vegetarianos, Opciones veganas, Opciones sin gluten',
+    'Características':'Comida para llevar, Asientos al aire libre, Asiento, Meseros, Acceso para silla de ruedas, Sirve alcohol, Wi-Fi gratis',
+    'Dónde': {
+      'Barranco':'Jirón Sucre 335A',
+    },
+    'Horario de Atención':'L-D de 8:00a.m. a 10:00p.m.',
+    'Teléfono':'(01) 2478804',
+    'Facebook':'https://www.facebook.com/labodegaverde/',
+    'Platos principales':['Campesina S/17.00','Zucchini tomete seco S/17.00','Pirámide de lúcuma S/13.00','Pie vegano limón S/9.00']
+  },
+  'Cerato Café':{
+    'Logo':'https://www.facebook.com/886798681436285/photos/a.886800291436124.1073741825.886798681436285/886800298102790/?type=1&theater',
+    'Tipo de comida': 'Café, Delicatessen',
+    'Características':'Especialidad son waffles y cremoladas. Se puede hacer reservas para celebrar el cumpleaños o fiestas especiales a nuestras mascotas.',
+    'Dónde': {
+      'Jesús María':'Avenida Húsares de Junín 561',
+    },
+    'Horario de Atención':'L-D de 9:00a.m. a 11:00p.m.',
+    'Teléfono':'934284068',
+    'Facebook':'https://www.facebook.com/Cerato-Caf%C3%A9-886798681436285/',
+    'Platos principales':['Waffles y Crepes S/8.00 a S/13.00','Smoothies S/9.00 a S/12.00','Sandwiches S/6.00 a S/16.00']
+  },
+}
